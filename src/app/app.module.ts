ba83a430be4b8709799cc576e475b17ecb31163a
jf.vallejo71 <jf.vallejo71@gmail.com>
2020-12-11 16:45:42 -0500

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer.component';
+import { HouseComponent } from './house/house.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    FooterComponent
+    FooterComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule,
